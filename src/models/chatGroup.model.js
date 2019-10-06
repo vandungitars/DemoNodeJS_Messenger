50eafe1f5c641216227c17357faaaf0d27d924ad
dung-lv <dung-lv@trente.asia>
2019-10-06 23:28:08 +0700

    create database model

---
+++ src/models/chatGroup.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let schema = mongoose.Schema;
+
+let ChatGroupSchema = new Schema({
+    name: String,
+    userAmount: {type: Number, min: 3, max: 177},
+    messageAmount: {type: Number, default: 0},
+    userId: String,
+    members: [
+        {userId: String}
+    ],
+    text: String,
+    file: {data: Buffer, contentType: String, fileName: String},
+    createAt: {type: Number, default: Date.now},
+    updateAt: {type: Number, default: null},
+    deleteAt: {type: Number, default: null}
+});
+module.exports = mongoose.model("chat-group", ChatGroupSchema);
---
+++ src/models/notification.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+let schema = mongoose.Schema;
+
+let NotificationSchema = new Schema({
+    sender: {
+        id: String,
+        username: String,
+        avatar: String
+    },
+    receiver: {
+        id: String,
+        username: String,
+        avatar: String
+    },
+    type: String,
+    content: String,
+    isRead: {type: Boolean, default: false},
+    createAt: {type: Number, default: Date.now}
+});
+module.exports = mongoose.model("notification", NotificationSchema);
